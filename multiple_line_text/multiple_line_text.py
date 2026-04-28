6e3ff18ab104908f3d9e53265f3d71ae99f40f5e
Dan <danbertbacer@gmail.com>
2026-04-28 22:46:02 +0800

    the code is working and saving in mylife.txt file

--- multiple_line_text/multiple_line_text.py
+++ multiple_line_text/multiple_line_text.py
@@ -1,5 +1,13 @@
-def write_multiple_lines(filename="mylife.txt"):
+def write_multiple_lines(filename='mylife.txt'):
     with open(filename, 'w') as file:
         while True:
             line = input("Enter line: ")
             file.write(line + "\n")
+
+            choice = input("Are there more lines y/n? ").lower()
+            if choice != 'y':
+                break
+
+    print(f"All lines successfully saved to {filename}")
+
+write_multiple_lines()
